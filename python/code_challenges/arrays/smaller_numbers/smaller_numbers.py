30e0f13b513c977b8b24cc8829233d547ee4834b
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-22 12:45:54 -0600

    fixed test typo

--- python/code_challenges/arrays/smaller_numbers/smaller_numbers.py
+++ python/code_challenges/arrays/smaller_numbers/smaller_numbers.py
@@ -1,10 +1,7 @@
-from typing import List
-
-class Solution:
-    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
-        result = []
-        tempNums = nums.copy()
-        nums.sort()
-        for n in tempNums:
-            result.append(nums.index(n))
-        return result
+def smallerNumbersThanCurrent(nums):
+    result = []
+    tempNums = nums.copy()
+    nums.sort()
+    for n in tempNums:
+        result.append(nums.index(n))
+    return result
